796142d97bc730abe47d3739f94f57f7b64dccf4
Akhil M Anil <70442264+akhil-1998@users.noreply.github.com>
2021-08-31 20:16:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,21 +8,8 @@ import WeavyApp from './weavy/WeavyApp';
 
 export default class App extends Component {
   async getJwt() {
-    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJteWNsaWVudGlkIiwibmFtZSI6ImFraGlsIiwiZW1haWwiOiJha2hpbG1hbmlsMTIzQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiQWtoaWxAMTIzIiwic3ViIjoibXljbGllbnRpZCIsImNsaWVudF9pZCI6Im15Y2xpZW50aWQiLCJjbGllbnRfc2VjcmV0IjoibXljbGllbnRzZWNyZXQiLCJuYmYiOjE2MzA0MTgwNjcsImV4cCI6MTYzMDQyMTY2N30.zBVKA8R1IFzZWB-EuoFpv2kkxtsYol71wz9okC0sNQY';
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJteWNsaWVudGlkIiwibmFtZSI6ImFrc2hheSIsInBhc3N3b3JkIjoiYWtzaGF5QDk3Iiwic3ViIjoibXljbGllbnRpZCIsImNsaWVudF9pZCI6Im15Y2xpZW50aWQiLCJjbGllbnRfc2VjcmV0IjoibXljbGllbnRzZWNyZXQiLCJuYmYiOjE2MzA0MjA4OTUsImV4cCI6MTYzMDQyNDQ5NX0.Th7y8Aki1dePQwTFwLstnVZGQjjEwbhzMHSoH7YF61o';
   }
-  // async getJwt() {
-
-  //   var token = new JwtBuilder()
-  //   .WithAlgorithm(new HMACSHA256Algorithm())    
-  //   .AddClaim("exp", DateTimeOffset.UtcNow.AddSeconds(60).ToUnixTimeSeconds())    
-  //   .AddClaim("iss", "weavy")
-  //   .AddClaim("sub", "c134c50a-249b-4b3c-b8b8-158be2219795")
-  //   .AddClaim("client_id", "c134c50a-249b-4b3c-b8b8-158be2219795")
-  //   .WithSecret("lGXyY/RifcbKKC[_8Bircb[@f]W0BC=c")
-  //   .Encode();
-
-  //   return token;
-  // }
 
   render() {
     return (
